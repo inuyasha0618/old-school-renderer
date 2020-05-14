450bfdbba3036533d40f2d83b82c5a257a481e77
inuyasha0618 <chenjiandiwang@qq.com>
2020-05-14 23:33:11 +0800

    add MVP and Viewport transform

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "tgaimage.h"
 #include "model.h"
+#include <math.h>
 
 // const TGAColor white = TGAColor(255, 255, 255, 255);
 // const TGAColor red   = TGAColor(255, 0,   0,   255);
@@ -70,11 +71,56 @@ void rasterization(tvec2<int> *pts, TGAImage& image, TGAColor color)
 	}
 }
 
+mat4 getPerspective(float FOV, float aspect, float near, float far)
+{
+	return mat4(
+		1.0 / (aspect * tan(radians(FOV * 0.5))), 0.0, 0.0, 0.0,
+		0.0, 1.0 / tan(radians(FOV * 0.5)), 0.0, 0.0,
+		0.0, 0.0, (near + far) / (near - far), -1.0,
+		0.0, 0.0, 2.0 * near * far / (near - far), 0.0 
+	);
+}
+
+mat4 getViewMatrix(vec3& eye, vec3& target, vec3& up)
+{
+	vec3 Z = normalize(eye - target);
+	vec3 X = cross(normalize(up), Z);
+	vec3 Y = cross(Z, X);
+
+	mat4 View2World = mat4(
+		X.x, X.y, X.z, 0.0,
+		Y.x, Y.y, Y.z, 0.0,
+		Z.x, Z.y, Z.z, 0.0,
+		eye.x, eye.y, eye.z, 1.0
+	);
+
+	mat3 A = mat3(
+		X.x, Y.x, Z.x,
+		X.y, Y.y, Z.y,
+		X.z, Y.z, Z.z
+	);
+
+	vec3 right_col = -A * eye;
+
+	return mat4(
+		X.x, Y.x, Z.x, 0.0,
+		X.y, Y.y, Z.y, 0.0,
+		X.z, Y.z, Z.z, 0.0,
+		right_col.x, right_col.y, right_col.z, 1.0
+	);
+}
+
+tvec2<int> viewport(vec3 NDC, int width, int height)
+{
+	int x = int((NDC.x + 1.0) * 0.5 * width);
+	int y = int((NDC.y + 1.0) * 0.5 * height);
+	return tvec2<int>(x, y);
+}
 // glm构造矩阵式按列传入的， 并且tranlate rotate等是在当前的姿态基础上施加变换
 int main(int argc, char** argv) {
-	const int width = 800;
-	const int height = 800;
-	TGAImage image(width, height, TGAImage::RGB);
+	const int ScreenWidth = 800;
+	const int ScreenHeight = 800;
+	TGAImage image(ScreenWidth, ScreenHeight, TGAImage::RGB);
 	// glm::vec2 pts[3] = {vec2(10.0, 10.0), vec2(100.0, 30.0), vec2(190.0, 160.0)};
 	// rasterization(pts, image, TGAColor(255, 255, 0, 255));
 
@@ -87,16 +133,28 @@ int main(int argc, char** argv) {
 		model = new Model("./african_head.obj");
 	}
 	
+	vec3 eye = vec3(0.0, 0.0, 2.0);
+	vec3 target = vec3(0.0, 0.0, 0.0);
+	vec3 up = vec3(0.0, 1.0, 0.0);
+	mat4 ViewMatrix = getViewMatrix(eye, target, up);
+	mat4 PerspectiveMatrix = getPerspective(90, 1.0, 0.1, 20);
+
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		vector<int> vert_idx_in_currentFace = model->face(i);
 		tvec2<int> screen_pos[3];
 		for (int j = 0; j < 3; j++)
 		{
-			// screen_pos[j].x = int((model->vert(vert_idx_in_currentFace[j]).x + 1) * 0.5 * width);
-			// screen_pos[j].y = int((model->vert(vert_idx_in_currentFace[j]).y + 1) * 0.5 * height);
+			// screen_pos[j].x = int((model->vert(vert_idx_in_currentFace[j]).x + 1) * 0.5 * ScreenWidth);
+			// screen_pos[j].y = int((model->vert(vert_idx_in_currentFace[j]).y + 1) * 0.5 * ScreenHeight);
+			vec3 pos = model->vert(vert_idx_in_currentFace[j]);
+			vec4 worldPos = vec4(pos.x, pos.y, pos.z, 1.0);
+			vec4 viewPos = ViewMatrix * worldPos;
+			vec4 clipPos = PerspectiveMatrix * viewPos;
+			vec3 NDC = vec3(clipPos.x / clipPos.w, clipPos.y / clipPos.w, clipPos.z / clipPos.w);
 
-			screen_pos[j] = tvec2<int>((model->vert(vert_idx_in_currentFace[j]).x + 1) * 0.5 * width, (model->vert(vert_idx_in_currentFace[j]).y + 1) * 0.5 * height);
+			screen_pos[j] = viewport(NDC, ScreenWidth, ScreenHeight);
+			// screen_pos[j] = tvec2<int>((model->vert(vert_idx_in_currentFace[j]).x + 1) * 0.5 * ScreenWidth, (model->vert(vert_idx_in_currentFace[j]).y + 1) * 0.5 * ScreenHeight);
 		}
 
 		// rasterization(screen_pos, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
